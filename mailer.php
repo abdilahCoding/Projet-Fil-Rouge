de902d59ebb0c0baacdde0b557ba2403e4f92d20
abdilahCoding <57251427+abdilahCoding@users.noreply.github.com>
2020-09-03 12:39:25 +0100

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -27,7 +27,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'abdilahelaidii@gmail.com';                     // SMTP username
-    $mail->Password   = 'yjrpoavmhumkqvot';                               // SMTP password
+    $mail->Password   = 'xcffffgg';                               // SMTP password
     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
